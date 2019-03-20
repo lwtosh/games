f5174887c5fea378240afef693ba1716d52b9f22
binfei <1615077717@qq.com>
2019-03-20 19:33:21 +0800

    提交

---
+++ pages/process/process.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    time: "10.00",
+    // flag: false,
+    imgUrl: ['http://10168245.h40.faiusr.com/4/11/ACgIABAEGAAg8_PpyQUo-aWmrwYwpgE4yAE.png'],
+  },
+  tiao: function() {
+    var imgUrl1 = 'http://10168245.h40.faiusr.com/4/11/ACgIABAEGAAg9uPpyQUogO_luAMwpgE4yAE.png'
+    this.setData({
+    })
+  },
+  onLoad: function(options) {}
+})
